0cbde47a3e70042fbb0bdd1060d0fa9eb0fcc147
Latsuj <justal.kevin@gmail.com>
2020-11-29 10:27:25 +0800

    [CONFIG] Fixing the linting error

--- App.tsx
+++ App.tsx
@@ -1,23 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const white = "#fff";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World, I'm Justal Kevin</Text>
-	  	<Text>I'm a Fullstack Developer</Text>
-  	  	<Text>If any questions, contact me at justal.kevin@gmail.com</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text>Hello World, Im Justal Kevin</Text>
+			<Text>Im a Fullstack Developer</Text>
+			<Text>If any questions, contact me at justal.kevin@gmail.com</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: white,
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
