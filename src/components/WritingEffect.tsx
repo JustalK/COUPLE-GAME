3137dbc956d71aa1bac02aa141628e93a00fb297
Latsuj <justal.kevin@gmail.com>
2020-11-30 12:40:07 +0800

    [FEATURE] Adding font

--- src/components/WritingEffect.tsx
+++ src/components/WritingEffect.tsx
@@ -5,19 +5,22 @@ import { colors } from '../consts/colors'
 export default WritingEffect = (props) => {
 	const [fullText, setFullText] = useState("");
 	const [startAnimation, setStartAnimation] = useState(true);
+	const [indexArray, setIndexArray] = useState(0);
 
-	function writing(actualText) {
+	function writing(actualText, indexActualArray) {
 		const i = actualText.length;
-		if (i < props.data.length) {
-			const newFullText = actualText  + props.data.charAt(i);
+		if (i < props.data[indexActualArray].length) {
+			const newFullText = actualText  + props.data[indexActualArray].charAt(i);
 			setFullText(newFullText);
 			setTimeout(function() {
-				writing(newFullText)
+				writing(newFullText, indexActualArray)
 			}, 100);
 		} else {
 			setTimeout(function() {
+				const newIndexArray = (indexActualArray + 1)%props.data.length
+				setIndexArray(newIndexArray);
 				setFullText("");
-				writing("");
+				writing("", newIndexArray);
 			}, 1000);
 		}
 	}
@@ -25,7 +28,7 @@ export default WritingEffect = (props) => {
 	if(startAnimation) {
 		setStartAnimation(false);
 		setTimeout(function() {
-			writing(fullText)
+			writing(fullText, indexArray)
 		}, 100);
 	}
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,9 +6,20 @@ import { colors } from '../consts/colors'
 import { styleText } from '../styles/main'
 import BlinkingEffect from '../components/BlinkingEffect'
 import WritingEffect from '../components/WritingEffect'
+import { useFonts } from 'expo-font';
 
 export default function Home() {
-	const jobs = ["Fullstack Developer", "Backend Developer", "Frontend Developer"]
+	const jobs = ["Fullstack Developer", "Backend Developer", "Frontend Developer"];
+
+	const [loaded] = useFonts({
+		Lato: require('../../assets/fonts/Lato-Regular.ttf'),
+		Heebo: require('../../assets/fonts/Heebo-Bold.ttf'),
+	  });
+
+	  if (!loaded) {
+	    return null;
+	  }
+
 	return (
 		<View style={styles.container}>
 			<Text style={styles.textStyle}>Hello World, Im Justal Kevin</Text>
@@ -31,6 +42,7 @@ const styles = StyleSheet.create({
 	},
 	textStyle: {
 		fontSize: 15,
+		fontFamily: "Lato",
 		color: colors.cyan
 	},
 	textCursor: {
